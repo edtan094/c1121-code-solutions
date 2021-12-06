1d10205292247a885717d26138b3b805a4c8d260
Edwin Tan <etan094@gmail.com>
2021-12-06 10:53:59 -0800

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+var fullName = 'Edwin Tan';
+var isCool = true;
+var totalPets = 1;
+
+console.log(fullName);
+console.log(isCool);
+console.log(totalPets);
+
+console.log(typeof fullName);
+console.log(typeof isCool);
+console.log(typeof totalPets);
+
+var unknown = null;
+console.log(unknown);
+console.log(typeof unknown);
+
+var never;
+console.log(never);
+console.log(typeof never);
