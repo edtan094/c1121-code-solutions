864a790c4db4fcc1f8b243a8db2621a63830114a
Edwin Tan <etan094@gmail.com>
2021-12-07 14:34:17 -0800

    Added some arrays to object properties to keep formatting consistent.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,39 +1,39 @@
 var orderHistory = [
   {
-    item: 'Javascript for impatient programmers',
-    author: 'Rauschmayer, Dr. Axel',
-    itemCost: 31.55,
+    item: ['Javascript for impatient programmers'],
+    author: ['Rauschmayer, Dr. Axel'],
+    itemCost: [31.55],
     orderTotal: 34,
     orderPlaced: 'August 4, 2020',
     delivered: 'August 8, 2020',
     returnWindow: 'September 7, 2020',
     shipTo: 'JS Masher',
     orderNumber: '114-394',
-    image: 'url javascript'
+    image: ['url javascript']
   },
   {
-    item: 'The Timeless Way of Building',
-    author: 'Alexander, Christopher',
-    itemCost: 41.33,
+    item: ['The Timeless Way of Building'],
+    author: ['Alexander, Christopher'],
+    itemCost: [41.33],
     orderTotal: 44.53,
     orderPlaced: 'July 19, 2020',
     delivered: 'July 20, 2020',
     returnwindow: 'August 19, 2020',
     shipTo: 'JS Masher',
     orderNumber: '113-998',
-    image: 'url the timeless way of building'
+    image: ['url the timeless way of building']
   },
   {
-    item: 'Gamecube Controller Adapter',
-    author: ' ',
-    itemCost: 17.22,
+    item: ['Gamecube Controller Adapter'],
+    author: [' '],
+    itemCost: [17.22],
     orderTotal: 15.98,
     orderPlaced: 'July 4, 2020',
     delivered: 'July 7, 2020',
     returnWindow: 'August 5, 2020',
     shipTo: 'JS Masher',
     orderNumber: '114-287',
-    image: 'url gamecube controller adapter'
+    image: ['url gamecube controller adapter']
   },
   {
     item: ['Gamecube Controller', 'The Art of Sql'],
