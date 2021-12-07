c605e45c4529648f737417ccd46bdea03ae92560
Edwin Tan <etan094@gmail.com>
2021-12-06 17:57:27 -0800

    Completed javascript-arrays exercise.

---
+++ javascript-arrays/main.js
@@ -0,0 +1,16 @@
+var colors = ['red', 'white', 'blue'];
+console.log('value of colors[0]:', colors[0]);
+console.log('value of colors[1]', colors[1]);
+console.log('value of colors[2]', colors[2]);
+console.log('America is ' + colors[0] + ' , ' + colors[1] + ' , and ' + colors[2]);
+console.log(colors);
+colors[2] = 'green';
+console.log('Mexico is ' + colors[0] + ' , ' + colors[1] + ' , and ' + colors[2]);
+
+var students = ['Edwin', 'James', 'Kevin', 'Ashley'];
+var numberOfStudents = students.length;
+console.log('numberOfStudents:', numberOfStudents);
+var lastIndex = numberOfStudents - 1;
+var lastStudent = students[lastIndex];
+console.log('lastStudent:', lastStudent);
+console.log('students:', students);
