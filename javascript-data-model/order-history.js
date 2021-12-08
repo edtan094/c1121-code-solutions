19a75329cd3e185adffe3f8dbba081ccc59f6da3
Edwin Tan <etan094@gmail.com>
2021-12-07 16:01:32 -0800

    Added arrays for one property called bought.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,53 +1,72 @@
 var orderHistory = [
   {
-    item: ['Javascript for impatient programmers'],
-    author: ['Rauschmayer, Dr. Axel'],
-    itemCost: [31.55],
+    bought: [
+      {
+        item: 'Javascript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        itemCost: 31.55,
+        image: 'url javascript'
+      }
+    ],
     orderTotal: 34,
     orderPlaced: 'August 4, 2020',
     delivered: 'August 8, 2020',
     returnWindow: 'September 7, 2020',
     shipTo: 'JS Masher',
-    orderNumber: '114-394',
-    image: ['url javascript']
+    orderNumber: '114-394'
   },
   {
-    item: ['The Timeless Way of Building'],
-    author: ['Alexander, Christopher'],
-    itemCost: [41.33],
+    bought: [
+      {
+        item: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        itemCost: 41.33,
+        image: 'url the timeless way of building'
+      }
+    ],
     orderTotal: 44.53,
     orderPlaced: 'July 19, 2020',
     delivered: 'July 20, 2020',
-    returnwindow: 'August 19, 2020',
+    returnWindow: 'August 19, 2020',
     shipTo: 'JS Masher',
-    orderNumber: '113-998',
-    image: ['url the timeless way of building']
+    orderNumber: '113-998'
   },
   {
-    item: ['Gamecube Controller Adapter'],
-    author: [' '],
-    itemCost: [17.22],
+    bought: [
+      {
+        item: 'Gamecube Controller Adapter',
+        author: null,
+        itemCost: 17.22,
+        image: 'url gamecube controller adapter'
+      }
+    ],
     orderTotal: 15.98,
     orderPlaced: 'July 4, 2020',
     delivered: 'July 7, 2020',
     returnWindow: 'August 5, 2020',
     shipTo: 'JS Masher',
-    orderNumber: '114-287',
-    image: ['url gamecube controller adapter']
+    orderNumber: '114-287'
   },
   {
-    item: ['Gamecube Controller', 'The Art of Sql'],
-    author: [' ', 'Faroult, Stephane'],
-    itemCost: [94.95, 33.99],
+    bought: [
+      {
+        item: 'Gamecube Controller',
+        author: null,
+        itemCost: 94.95,
+        image: 'url gamecube controller'
+      },
+      {
+        item: 'The Art of Sql',
+        author: 'Faroult, Stephane',
+        itemCost: 33.99,
+        image: 'url The Art of Sql'
+      }
+    ],
     orderTotal: 138.93,
     orderPlaced: 'July 3, 2020',
     delivered: 'July 5, 2020',
-    returnwindow: 'August 4, 2020',
+    returnWindow: 'August 4, 2020',
     shipTo: 'JS Masher',
-    orderNumber: '113-288',
-    image: ['url gamecube controller', 'url The Art of Sql']
+    orderNumber: '113-288'
   }
 ];
-
-console.log(orderHistory[2].itemCost);
-console.log(orderHistory[3].item[1]);
