f72f06d960896c5f0ff37498a4b7a0cc03208181
Edwin Tan <etan094@gmail.com>
2021-12-10 21:47:43 -0800

    Completed array-manipulation-1 exercise.

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,1 +1,18 @@
 /* exported compact */
+// function: compact
+// argument: array
+// return: an array containing on elements that are not falsy but still left in order
+
+// create a storage for a list of elements
+// for each item on the list that is not a falsy, put it into the storage list
+// produce storage list
+
+function compact(array) {
+  var newArray = [];
+  for (var i = 0; i <= array.length - 1; i++) {
+    if (array[i]) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
