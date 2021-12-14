eaaa48964dbb4f3ed39ced7929b8f1edfbd9816e
Edwin Tan <etan094@gmail.com>
2021-12-14 11:30:19 -0800

    In progress.

--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,1 +1,16 @@
 /* exported ransomCase */
+// function: ransomCase
+// argument: any string
+// return: returns string with every other character uppercased
+
+// take the argument and lowercase all the letters and store it in another storage space
+// for every other letter beginning with the second letter, uppercase that letter
+// produce the storage space
+
+function ransomCase(string) {
+  var newWord = string.toLowerCase();
+  for (var i = 1; i <= newWord.length - 1; i += 2) {
+    newWord[i] = newWord[i].toUpperCase();
+  }
+  return newWord;
+}
