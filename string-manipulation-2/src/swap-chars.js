f3555a775da05672891257adbf637b289ddbf5a7
Edwin Tan <etan094@gmail.com>
2021-12-15 13:30:20 -0800

    In progress

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,16 @@
 /* exported swapChars */
+// function: swapChars
+// argument: firstIndex: an integral number
+//          secondIndex: an integral number
+//           string
+// return: string with the characters at firstIndex and secondIndex swapped.
+
+// create a storage both for the first and second character
+// find first character on the string using the first integral number
+// find the second character on the string using the second integral number
+// switch their locations on the string around
+// return string
+
+function swapChars(firstIndex, secondIndex, string) {
+
+}
