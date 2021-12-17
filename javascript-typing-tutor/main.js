1d463404451dc8993da67ceba867f269cc0c4e04
Edwin Tan <etan094@gmail.com>
2021-12-16 17:54:40 -0800

    In progress.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,2 +1,14 @@
-var $heading = document.querySelector('h1');
-$heading.addEventListener('keydown');
+// var $heading = document.querySelector('h1');
+// var $span = document.querySelectorAll('span');
+// $heading.addEventListener('keydown', function () {
+
+//   // for (var i = 0; i < $span.length; i++){
+//   //   if ($span[i] === "keydown")
+//   }
+
+// });
+
+// the first letter should start with an underscore
+// for each letter that is typed, highlight green without any underscore.
+// if the letter is typed incorrectly highlight red with an underscore
+// if the letter is typed correctly, move the underscore to the next letter
