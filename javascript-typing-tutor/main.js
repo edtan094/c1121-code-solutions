7f08b8bf2804a6d6eb78a38fa71b2e8f65e0ead0
Edwin Tan <etan094@gmail.com>
2021-12-16 22:50:16 -0800

    In progress.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,14 +1,24 @@
-// var $heading = document.querySelector('h1');
-// var $span = document.querySelectorAll('span');
-// $heading.addEventListener('keydown', function () {
-
-//   // for (var i = 0; i < $span.length; i++){
-//   //   if ($span[i] === "keydown")
-//   }
-
-// });
+var $heading = document.querySelector('h1');
+var $span = document.querySelectorAll('span');
+// $heading.addEventListener('keydown', myFunction)
+document.addEventListener('keydown', myFunction)
+function myFunction (event){
+  console.log(event)
+  console.log(event.target)
+  console.log(event.target.tagName)
+  if(event.key === $span[0].textContent){
+    console.log("Hurray")
+  }
+  // if (event[key] === span[0])
+  // for (var i = 0; i < $span.length; i++){
+  //   if (event[key] === span[i] ){
+  //     true
+  //   }
+  // }
+}
 
 // the first letter should start with an underscore
 // for each letter that is typed, highlight green without any underscore.
 // if the letter is typed incorrectly highlight red with an underscore
 // if the letter is typed correctly, move the underscore to the next letter
+// probably need a boolean (true and false) to determine whether the text is correct
