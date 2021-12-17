586b102b472b62c4c646e83f44db840ee3f8ebe2
Edwin Tan <etan094@gmail.com>
2021-12-17 11:03:44 -0800

    Changed variable names of i and j in the loops.

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -5,14 +5,14 @@ var $viewElements = document.querySelectorAll('.view');
 $tabContainer.addEventListener('click', function (event) {
   if (event.target.matches('.tab')) {
     var $dataView = event.target.getAttribute('data-view');
-    for (var i = 0; i < $viewElements.length; i++) {
-      $viewElements[i].className = 'hidden';
-      if ($dataView === $viewElements[i].getAttribute('data-view')) {
-        $viewElements[i].classList.replace('hidden', 'view');
+    for (var viewElementsPage = 0; viewElementsPage < $viewElements.length; viewElementsPage++) {
+      $viewElements[viewElementsPage].className = 'hidden';
+      if ($dataView === $viewElements[viewElementsPage].getAttribute('data-view')) {
+        $viewElements[viewElementsPage].classList.replace('hidden', 'view');
       }
     }
-    for (var j = 0; j < $tabElements.length; j++) {
-      $tabElements[j].classList.remove('active');
+    for (var viewTab = 0; viewTab < $tabElements.length; viewTab++) {
+      $tabElements[viewTab].classList.remove('active');
     }
 
     event.target.classList.add('active');
