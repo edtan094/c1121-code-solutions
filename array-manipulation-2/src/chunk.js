4a33715077d96d9f575581eaa02dbce42642c3f3
Edwin Tan <etan094@gmail.com>
2021-12-18 13:56:31 -0800

    Completed array-manipulation-2 except for the last one.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,20 @@
 /* exported chunk */
+// function : chunk
+// argument: array
+//           size - a positive integer number indicating the desired chunk size
+//  return: A new Array of Arrays. Each sub-Array has a length equal to size. Remaining elements are gathered into a final sub-Array.
+
+// if the size is greater than the length of the array, return an empty array
+// Starting from the beginning of the array, push these items into a new array with the size of the new array based on the size provided in the argument
+// once the size of the array has been hit, create a new array if there are more items left
+// return all the arrays
+// function chunk(array, size) {
+//   var newArray = [];
+//   if (size > array.length) {
+//     return newArray;
+//   }
+//   for (var i = 0; i < array.length; i += size) {
+//     newArray = array.slice(i, size);
+//   }
+//   return newArray;
+// }
--- array-manipulation-2/src/includes.js
+++ array-manipulation-2/src/includes.js
@@ -1,1 +1,20 @@
 /* exported includes */
+// function : includes
+// argument: array
+//           value - the value to search for in array
+// return : boolean indicating the presecense of value at any index of array
+
+// for each item in the array, if any of the items matches the value in the array, return true, otherwise it is false
+// if the array is empty, return false
+
+function includes(array, value) {
+  if (array === []) {
+    return false;
+  }
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      return true;
+    }
+  }
+  return false;
+}
