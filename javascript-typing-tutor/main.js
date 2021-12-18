d9a077885e487f3dd246237dc58c7eb7ae86ddaf
Edwin Tan <etan094@gmail.com>
2021-12-17 21:28:56 -0800

    Completed javascript-typing-tutor exercise.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -2,9 +2,6 @@ var $span = document.querySelectorAll('span');
 var spanElements = 0;
 document.addEventListener('keydown', myFunction);
 function myFunction(event) {
-  console.log(event);
-  console.log(event.target);
-  console.log(event.target.tagName);
   if (event.key !== $span[spanElements].textContent) {
     $span[spanElements].className = 'red-color';
     return;
@@ -15,9 +12,3 @@ function myFunction(event) {
   }
   spanElements++;
 }
-
-// the first letter should start with an underscore
-// for each letter that is typed, highlight green without any underscore.
-// if the letter is typed incorrectly highlight red with an underscore
-// if the letter is typed correctly, move the underscore to the next letter
-// probably need a boolean (true and false) to determine whether the text is correct
