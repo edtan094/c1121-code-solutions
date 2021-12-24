0415f2c1b983bfbe2efec9370886ca5302b100b5
Edwin Tan <etan094@gmail.com>
2021-12-24 14:43:09 -0800

    Completed object-manipulation-2 exercise.

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -6,8 +6,10 @@
 // If a key is listed in keys, then it should not be included in the new Object.
 
 // create a variable for an object that will contain the properties and property value of whatever is currently not listed in the key
-// for each key that matches the property key in the source object, do not pass this property key and value to the new object
-//
+// for each property key and property value within the source, push it into the array
+// for each property key within keys that matches a value within array, remove the property key and value from array
+// once that it is done, push the remaining values in array into object
+// produce the object
 function omit(source, keys) {
   var object = {};
   var array = [];
@@ -15,20 +17,17 @@ function omit(source, keys) {
     array.push(prop);
     array.push(source[prop]);
   }
-  for (var i = 0; i < array.length; i++) {
-    if (array[i + 1] !== source[array[i]] && i % 2 === 0) {
-      object[array[i]] = array[i + 1];
+  for (var i = 0; i < keys.length; i++) {
+    for (var j = 0; j < array.length; j++) {
+      if (keys[i] === array[j]) {
+        array.splice(j, 2);
+      }
+    }
+  }
+  for (var k = 0; k < array.length; k++) {
+    if (k % 2 === 0) {
+      object[array[k]] = source[array[k]];
     }
   }
   return object;
 }
-// function pick(source, keys) {
-//   var object = {};
-//   for (var i = 0; i < keys.length; i++) {
-//     var keyIndex = keys[i];
-//     if (source[keyIndex] !== undefined) {
-//       object[keyIndex] = source[keyIndex];
-//     }
-//   }
-//   return object;
-// }
