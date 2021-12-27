f67cdf1a3bc27f1149b5872179b7e19ee0941ad5
Edwin Tan <etan094@gmail.com>
2021-12-27 13:04:34 -0800

    Working on chunk.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -4,9 +4,10 @@
 //           size - a positive integer number indicating the desired chunk size
 //  return: A new Array of Arrays. Each sub-Array has a length equal to size. Remaining elements are gathered into a final sub-Array.
 
+// create a storage for all subarray
 // if the size is greater than the length of the array, return an empty array
-// Starting from the beginning of the array, push these items into a new array with the size of the new array based on the size provided in the argument
-// once the size of the array has been hit
+// Starting from the beginning of the array, push each items into a new array up to a length of the size given
+// once the size of the array has been hit, add it to a storage for sublists
 // create a new array if there are more items left and repeat step 2 starting with the item we have left off previously
 // return all the arrays
 function chunk(array, size) {
