847fa0cb2739111d7e8098c6592e85ff2f50eb2d
Edwin Tan <etan094@gmail.com>
2021-12-27 12:44:34 -0800

    In progress.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -6,15 +6,16 @@
 
 // if the size is greater than the length of the array, return an empty array
 // Starting from the beginning of the array, push these items into a new array with the size of the new array based on the size provided in the argument
-// once the size of the array has been hit, create a new array if there are more items left
+// once the size of the array has been hit
+// create a new array if there are more items left and repeat step 2 starting with the item we have left off previously
 // return all the arrays
-// function chunk(array, size) {
-//   if (array === []) {
-//     return array;
-//   }
-//   for (size; size < array.length; size * 2) {
-//     var newArray = [[]];
-//     newArray.unshift(array.slice(0, size));
-//   }
-//   return newArray;
-// }
+function chunk(array, size) {
+  if (array === []) {
+    return array;
+  }
+  for (size; size < array.length; size * 2) {
+    var newArray = [[]];
+    newArray.unshift(array.slice(0, size));
+  }
+  return newArray;
+}
