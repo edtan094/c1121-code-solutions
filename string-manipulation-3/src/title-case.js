1bcefae0dcc74f2384449955bb80535320d75bbd
edtan094 <etan094@gmail.com>
2021-12-31 15:19:50 -0800

    Working on titlecase.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -7,15 +7,29 @@ function titleCase(title) {
   var finalTitle = '';
   var finalProperTitle = '';
   for (var titleIndex = 0; titleIndex < title.length; titleIndex++) {
-    finalTitle += title[titleIndex];
+    if (titleIndex === 0) {
+      finalTitle += title[titleIndex].toUpperCase();
+    } else {
+      finalTitle += title[titleIndex].toLowerCase();
+    }
     if (title[titleIndex] === ' ' || titleIndex === title.length - 1) {
       for (var finalTitleIndex = 0; finalTitleIndex < finalTitle.length; finalTitleIndex++) {
         if (finalTitle.length > 3 && finalTitleIndex === 0) {
           finalProperTitle += finalTitle[0].toUpperCase();
-        } else {
+        } if (finalTitle[finalTitleIndex - 1] === '-') {
+          finalProperTitle += finalTitle[finalTitleIndex].toUpperCase();
+        } if (finalTitle[finalTitleIndex] === 'i' && finalTitle[finalTitleIndex + 1] === ' ') {
+          finalProperTitle += finalTitle[finalTitleIndex].toUpperCase();
+        } else if (finalTitle.length <= 3) {
+          finalProperTitle += finalTitle[finalTitleIndex];
+        } else if (finalTitleIndex !== 0) {
           finalProperTitle += finalTitle[finalTitleIndex];
         }
       }
     }
+    if (title[titleIndex] === ' ') {
+      finalTitle = '';
+    }
   }
+  return finalProperTitle;
 }
