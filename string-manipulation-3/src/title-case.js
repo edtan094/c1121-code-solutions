eee9ba2fd3034d1fa583795a4e2c68ca88216bf1
Edwin Tan <etan094@gmail.com>
2021-12-31 11:22:50 -0800

    Still working on title-case.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -2,3 +2,20 @@
 // function: titleCase
 // argument: title -  A JavaScript String representing a book title
 // return : The original title, but with APA Title Case Style applied.
+
+function titleCase(title) {
+  var finalTitle = '';
+  var finalProperTitle = '';
+  for (var titleIndex = 0; titleIndex < title.length; titleIndex++) {
+    finalTitle += title[titleIndex];
+    if (title[titleIndex] === ' ' || titleIndex === title.length - 1) {
+      for (var finalTitleIndex = 0; finalTitleIndex < finalTitle.length; finalTitleIndex++) {
+        if (finalTitle.length > 3 && finalTitleIndex === 0) {
+          finalProperTitle += finalTitle[0].toUpperCase();
+        } else {
+          finalProperTitle += finalTitle[finalTitleIndex];
+        }
+      }
+    }
+  }
+}
