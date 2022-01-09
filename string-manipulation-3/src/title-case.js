846503e641921a275344076a010b668819b6dfe6
edtan094 <etan094@gmail.com>
2022-01-08 16:33:34 -0800

    In progress.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -3,6 +3,39 @@
 // argument: title -  A JavaScript String representing a book title
 // return : The original title, but with APA Title Case Style applied.
 
+// function titleCase(title) {
+//   var finalTitle = '';
+//   var finalProperTitle = '';
+//   for (var titleIndex = 0; titleIndex < title.length; titleIndex++) {
+//     if (titleIndex === 0) {
+//       finalTitle += title[titleIndex].toUpperCase();
+//     } else {
+//       finalTitle += title[titleIndex].toLowerCase();
+//     }
+//     if (title[titleIndex] === ' ' || titleIndex === title.length - 1) {
+//       for (var finalTitleIndex = 0; finalTitleIndex < finalTitle.length; finalTitleIndex++) {
+//         if (finalTitle.length > 3 && finalTitleIndex === 0) {
+//           finalProperTitle += finalTitle[0].toUpperCase();
+//         } if (finalTitle[finalTitleIndex - 1] === '-') {
+//           finalProperTitle += finalTitle[finalTitleIndex].toUpperCase();
+//         } if (finalTitle[finalTitleIndex] === 'i' && finalTitle[finalTitleIndex + 1] === ' ') {
+//           finalProperTitle += finalTitle[finalTitleIndex].toUpperCase();
+//         } else if (finalTitle.includes('javascript') && finalTitleIndex === 4) {
+//           finalProperTitle += finalTitle[finalTitleIndex].toUpperCase();
+//         } else if (finalTitle.length <= 3) {
+//           finalProperTitle += finalTitle[finalTitleIndex];
+//         } else if (finalTitleIndex !== 0) {
+//           finalProperTitle += finalTitle[finalTitleIndex];
+//         }
+//       }
+//     }
+//     if (title[titleIndex] === ' ') {
+//       finalTitle = '';
+//     }
+//   }
+//   return finalProperTitle;
+// }
+
 function titleCase(title) {
   var finalTitle = '';
   var finalProperTitle = '';
@@ -14,14 +47,17 @@ function titleCase(title) {
     }
     if (title[titleIndex] === ' ' || titleIndex === title.length - 1) {
       for (var finalTitleIndex = 0; finalTitleIndex < finalTitle.length; finalTitleIndex++) {
-        if (finalTitle.length > 3 && finalTitleIndex === 0) {
+        if (finalTitle.length > 3 && finalTitleIndex === 0 && finalTitle !== 'and ' && finalTitle !== 'or ' && finalTitle !== 'nor ' && finalTitle !== 'but ' && finalTitle !== 'a ' && finalTitle !== 'an ' && finalTitle !== 'the ' && finalTitle !== 'as ' && finalTitle !== 'at ' && finalTitle !== 'by ' && finalTitle !== 'for ' && finalTitle !== 'in ' && finalTitle !== 'of ' && finalTitle !== 'on ' && finalTitle !== 'per ' && finalTitle !== 'to ') {
           finalProperTitle += finalTitle[0].toUpperCase();
-        } if (finalTitle[finalTitleIndex - 1] === '-') {
+        } else if (finalTitle.length <= 4 && finalTitle[finalTitle.length - 1] === ' ') {
+          finalProperTitle += finalTitle[finalTitleIndex];
+        }
+        if (finalTitle[finalTitleIndex - 1] === '-') {
           finalProperTitle += finalTitle[finalTitleIndex].toUpperCase();
         } if (finalTitle[finalTitleIndex] === 'i' && finalTitle[finalTitleIndex + 1] === ' ') {
           finalProperTitle += finalTitle[finalTitleIndex].toUpperCase();
-        } else if (finalTitle.length <= 3) {
-          finalProperTitle += finalTitle[finalTitleIndex];
+        } else if (finalTitle.includes('javascript') && finalTitleIndex === 4) {
+          finalProperTitle += finalTitle[finalTitleIndex].toUpperCase();
         } else if (finalTitleIndex !== 0) {
           finalProperTitle += finalTitle[finalTitleIndex];
         }
