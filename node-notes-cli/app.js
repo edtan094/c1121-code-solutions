6cc6b5bcf8fe5358ada26ea5e38ea854f57c157c
edtan094 <etan094@gmail.com>
2022-01-20 15:17:29 -0800

    Updated the read function to read only the property name and property values as strings.

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,8 +1,23 @@
 const fs = require('fs');
-
-if (process.argv[2] === 'read') {
+const json = require('./data.json');
+const appFunctions = process.argv[2];
+// const nodeArgument = process.argv[3];
+// const readFunction = fs.readFile('data.json', 'utf-8', (err, data) => {
+//   if (err) throw err;
+//   return data;
+// });
+if (appFunctions === 'read') {
   fs.readFile('data.json', 'utf-8', (err, data) => {
     if (err) throw err;
-    console.log(data);
+    for (const prop in json.notes) {
+      console.log(`${prop}: ${json.notes[prop]}`);
+    }
   });
 }
+// else if (appFunctions === 'create') {
+//   const existingText = readFunction();
+//   fs.writeFile('data.json', nodeArgument, 'utf-8', err => {
+//     if (err) throw err;
+//     console.log('Update Complete!');
+//   });
+// }
