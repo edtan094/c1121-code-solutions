28617e265df31017bd96cb8bb70be95c8d41c68e
edtan094 <etan094@gmail.com>
2022-01-20 19:46:43 -0800

    User can now add a note.

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,23 +1,17 @@
 const fs = require('fs');
 const json = require('./data.json');
 const appFunctions = process.argv[2];
-// const nodeArgument = process.argv[3];
-// const readFunction = fs.readFile('data.json', 'utf-8', (err, data) => {
-//   if (err) throw err;
-//   return data;
-// });
+const nodeArgument = process.argv[3];
 if (appFunctions === 'read') {
-  fs.readFile('data.json', 'utf-8', (err, data) => {
+  for (const prop in json.notes) {
+    console.log(`${prop}: ${json.notes[prop]}`);
+  }
+} else if (appFunctions === 'create') {
+  json.notes[json.nextId] = nodeArgument;
+  json.nextId++;
+  const data = JSON.stringify(json, null, 2);
+  fs.writeFile('data.json', data, 'utf-8', err => {
     if (err) throw err;
-    for (const prop in json.notes) {
-      console.log(`${prop}: ${json.notes[prop]}`);
-    }
+    console.log('Update Complete!');
   });
 }
-// else if (appFunctions === 'create') {
-//   const existingText = readFunction();
-//   fs.writeFile('data.json', nodeArgument, 'utf-8', err => {
-//     if (err) throw err;
-//     console.log('Update Complete!');
-//   });
-// }
