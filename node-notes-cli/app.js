77b0a4d4d3abc45588ed7492776edd680db3f0b3
edtan094 <etan094@gmail.com>
2022-01-20 21:45:44 -0800

    User can now delete a note.

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -14,4 +14,16 @@ if (appFunctions === 'read') {
     if (err) throw err;
     console.log('Update Complete!');
   });
+} else if (appFunctions === 'delete') {
+  for (let prop in json.notes) {
+    if (prop === nodeArgument) {
+      prop = parseInt(prop);
+      delete json.notes[prop];
+      const data = JSON.stringify(json, null, 2);
+      fs.writeFile('data.json', data, 'utf-8', err => {
+        if (err) throw err;
+        console.log('Deleted!');
+      });
+    }
+  }
 }
