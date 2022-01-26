6146486d3a5628c1ce9587bfe6a31516ec8291a8
edtan094 <etan094@gmail.com>
2022-01-25 21:52:40 -0800

    Clients can DELETE a note by id

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -17,20 +17,43 @@ app.get('/api/notes/:id', function (req, res) {
   if (Math.floor(id) !== id || id < 0) {
     res.status(400).json({ error: 'id must be a positive integer' });
   } else if (json.notes[id] === undefined) {
-    res.status(404).json({ error: 'id does not match any notes in the system' });
+    res.status(404).json({ error: `cannot find note with id ${id}` });
   } else if (json.notes[id] !== undefined) {
     res.status(200).json(json.notes[id]);
+  } else {
+    console.error(new Error());
+    res.sendStatus(500);
   }
 });
 
 app.post('/api/notes', function (req, res) {
   if (req.body === undefined) {
     res.status(400).json({ error: 'content is a required field' });
+  } if (req.body !== undefined) {
+    req.body.id = json.nextId;
+    json.notes[json.nextId] = req.body;
+    json.nextId++;
+    res.status(201).json(req.body);
+  } else {
+    console.error(new Error());
+    res.sendStatus(500);
   }
-  req.body.id = json.nextId;
-  json.notes[json.nextId] = req.body;
-  json.nextId++;
-  res.status(201).json(req.body);
+
+});
+
+app.delete('/api/notes/:id', function (req, res) {
+  const id = parseFloat(req.params.id);
+  if (Math.floor(id) !== id || id < 0) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (json.notes[id] === undefined) {
+    res.status(404).json({ error: `cannot find note with id ${id}` });
+  } else if (json.notes[id] !== undefined) {
+    delete json.notes[id];
+    res.status(204).json();
+  } else {
+    console.error(new Error());
+    res.sendStatus(500);
+  }
 });
 
 app.listen(3000, function () {
