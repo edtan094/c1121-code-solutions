db62e2388708dd9f402b678bae69f65b13a2308c
edtan094 <etan094@gmail.com>
2022-01-25 22:18:15 -0800

    Clients can replace a note by id.  There is still an issue where the conditional for no body is not working.  In progress.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,3 +1,4 @@
+// attempting to fix issues where req.body does not equal undefined but it actually equals an empty object when the body is empty????
 const express = require('express');
 const app = express();
 const json = require('./data.json');
@@ -27,7 +28,7 @@ app.get('/api/notes/:id', function (req, res) {
 });
 
 app.post('/api/notes', function (req, res) {
-  if (req.body === undefined) {
+  if (req.body === {}) {
     res.status(400).json({ error: 'content is a required field' });
   } if (req.body !== undefined) {
     req.body.id = json.nextId;
@@ -50,6 +51,24 @@ app.delete('/api/notes/:id', function (req, res) {
   } else if (json.notes[id] !== undefined) {
     delete json.notes[id];
     res.status(204).json();
+  } else {
+    console.error(new Error());
+    res.sendStatus(500);
+  }
+});
+
+app.put('/api/notes/:id', function (req, res) {
+  const id = parseFloat(req.params.id);
+  if (Math.floor(id) !== id || id < 0) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (req.body === {}) {
+    res.status(400).json({ error: 'content is a required field' });
+  } else if (json.notes[id] === undefined) {
+    res.status(404).json({ error: `cannot find note with id ${id}` });
+  } else if (json.notes[id] !== undefined) {
+    json.notes[id] = req.body;
+    json.notes[id].id = id;
+    res.status(200).json(req.body);
   } else {
     console.error(new Error());
     res.sendStatus(500);
