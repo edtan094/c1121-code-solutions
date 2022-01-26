2418ac8028d8f246cacaa5eeadcb819fe6fdc122
edtan094 <etan094@gmail.com>
2022-01-25 21:05:35 -0800

    Clients can GET a single note by id

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -10,6 +10,17 @@ app.get('/api/notes', function (req, res) {
   res.json(array);
 });
 
+app.get('/api/notes/:id', function (req, res) {
+  const id = parseFloat(req.params.id);
+  if (Math.floor(id) !== id || id < 0) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (json.notes[id] === undefined) {
+    res.status(404).json({ error: 'id does not match any notes in the system' });
+  } else if (json.notes[id] !== undefined) {
+    res.status(200).json(json.notes[id]);
+  }
+});
+
 app.listen(3000, function () {
   // eslint-disable-next-line no-console
   console.log('Listening on 3000!');
