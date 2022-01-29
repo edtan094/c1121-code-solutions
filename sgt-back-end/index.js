5ed2752416e0cc28d36d22ea72883f22142f1b4a
edtan094 <etan094@gmail.com>
2022-01-28 18:04:26 -0800

    User can get and post.

---
+++ sgt-back-end/index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const expressJSON = express.json();
+app.use(expressJSON);
+const pg = require('pg');
+const db = new pg.Pool({
+  connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.get('/api/grades', (req, res) => {
+  const sql = `
+  select *
+  from "grades"`;
+
+  db.query(sql)
+    .then(result => {
+      const grade = result.rows;
+      res.status(200).json(grade);
+    })
+
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
+      });
+    });
+});
+
+app.post('/api/grades', (req, res) => {
+  const body = req.body;
+  body.score = parseFloat(body.score);
+  if (body.name === undefined || body.course === undefined || body.score === undefined ||
+    Math.floor(body.score) !== body.score) {
+    res.status(400).json({
+      error: 'Missing name, course, score, or score is not an integer from 0 - 100'
+    });
+  }
+
+  const text = 'insert into grades(name, course, score) values($1, $2, $3) returning *';
+  const values = [body.name, body.course, body.score];
+  db.query(text, values)
+    .then(result => {
+      const grade = result.rows[0];
+      res.status(201).json(grade);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
+      });
+    });
+});
+
+// app.put();
+
+app.listen(3000, function () {
+  // eslint-disable-next-line no-console
+  console.log('Listening on 3000!');
+});
