c8492c648a997a8e40df7c28362c5aa62bcaadd5
edtan094 <etan094@gmail.com>
2022-02-07 11:58:18 -0800

    Added a const init variable with the property method.

--- fetch/main.js
+++ fetch/main.js
@@ -1,9 +1,12 @@
-fetch('https://jsonplaceholder.typicode.com/users')
+const init = {
+  method: 'GET'
+};
+fetch('https://jsonplaceholder.typicode.com/users', init)
   .then(response => response.json())
   .then(data => console.log(data))
   .catch(error => console.error(error));
 
-fetch('https://pokeapi.co/api/v2/pokemon/1')
+fetch('https://pokeapi.co/api/v2/pokemon/1', init)
   .then(response => response.json())
   .then(data => console.log(data))
   .catch(error => console.error(error));
