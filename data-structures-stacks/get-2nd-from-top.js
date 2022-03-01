cd9666b2aa2830b9977c959600750b67ae7da653
edtan094 <etan094@gmail.com>
2022-03-01 11:50:24 -0800

    Completed data-structures-stacks exercise.

--- data-structures-stacks/get-2nd-from-top.js
+++ data-structures-stacks/get-2nd-from-top.js
@@ -1,5 +1,18 @@
 /* exported get2ndFromTop */
 
 function get2ndFromTop(stack) {
-
+  if (stack.peek() === undefined) {
+    return undefined;
+  }
+  const onlyStack = stack.pop();
+  if (stack.peek() === undefined) {
+    stack.push(onlyStack);
+    return undefined;
+  }
+  stack.push(onlyStack);
+  const topStack = stack.pop();
+  const secondTopStack = stack.pop();
+  stack.push(secondTopStack);
+  stack.push(topStack);
+  return secondTopStack;
 }
--- data-structures-stacks/insert-under-top.js
+++ data-structures-stacks/insert-under-top.js
@@ -1,5 +1,10 @@
 /* exported insertUnderTop */
 
 function insertUnderTop(stack, value) {
-
+  if (stack.peek() === undefined) {
+    return undefined;
+  }
+  const topStack = stack.pop();
+  stack.push(value);
+  stack.push(topStack);
 }
