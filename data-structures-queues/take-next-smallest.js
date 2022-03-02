258fbcc68b695cc3e56cae4a072dd69b9334b58a
edtan094 <etan094@gmail.com>
2022-03-02 10:40:23 -0800

    Completed data-structures-queues exercise.

--- data-structures-queues/take-next-smallest.js
+++ data-structures-queues/take-next-smallest.js
@@ -1,21 +1,10 @@
 /* exported takeNextSmallest */
 
 function takeNextSmallest(queue) {
-  let item;
-  if (queue.peek() !== undefined) {
-    item = queue.dequeue();
-    if (queue.peek() === undefined) {
-      return item;
-    }
-    queue.enqueue(item);
-    item = queue.dequeue();
-    for (let i = 0; item === queue.peek(); i++) {
-      queue.enqueue(item);
-    }
+  let nextSmallest = queue.dequeue();
+  while (nextSmallest > queue.peek()) {
+    queue.enqueue(nextSmallest);
+    nextSmallest = queue.dequeue();
   }
+  return nextSmallest;
 }
-
-// take the first item and store it
-// use a for loop until we hit the same number again
-// if there is an item equal to or less than the first item, replace the first item with that number
-// all numbers that are greater than the smallest item are routed back to the queue
