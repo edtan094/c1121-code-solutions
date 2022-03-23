41ef225c8c79111eacd897839cdcbc79f0ea5993
edtan094 <etan094@gmail.com>
2022-03-23 15:26:44 -0700

    Refactored code to use hooks.

--- react-hot-button-component/src/hot-button.jsx
+++ react-hot-button-component/src/hot-button.jsx
@@ -1,51 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default class CustomButton extends React.Component {
-  constructor(prop) {
-    super(prop);
-    this.state = {
-      buttonCounter: 0
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+export default function HotButton(props) {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>Hello there! You click {count} times</p>
+      <ColorCounter counter={count} setCount={() => setCount(count + 1)}/>
+    </div>
+  );
+}
 
-  render() {
-    if (this.state.buttonCounter >= 18) {
-      return <button onClick={this.handleClick} className={this.hotButton()}>Hot Button</button>;
-    } else if (this.state.buttonCounter >= 15) {
-      return <button onClick={this.handleClick} className={this.hotButton()}>Hot Button</button>;
-    } else if (this.state.buttonCounter >= 12) {
-      return <button onClick={this.handleClick} className={this.hotButton()}>Hot Button</button>;
-    } else if (this.state.buttonCounter >= 9) {
-      return <button onClick={this.handleClick} className={this.hotButton()}>Hot Button</button>;
-    } else if (this.state.buttonCounter >= 6) {
-      return <button onClick={this.handleClick} className={this.hotButton()}>Hot Button</button>;
-    } else if (this.state.buttonCounter >= 3) {
-      return <button onClick={this.handleClick} className={this.hotButton()}>Hot Button</button>;
-    } else { return <button onClick={this.handleClick} className={this.hotButton()}>Hot Button</button>; }
-  }
-
-  hotButton() {
-    if (this.state.buttonCounter >= 18) {
-      return 'eighteen';
-    } else if (this.state.buttonCounter >= 15) {
-      return 'fifteen';
-    } else if (this.state.buttonCounter >= 12) {
-      return 'twelve';
-    } else if (this.state.buttonCounter >= 9) {
-      return 'nine';
-    } else if (this.state.buttonCounter >= 6) {
-      return 'six';
-    } else if (this.state.buttonCounter >= 3) {
-      return 'three';
-    }
-  }
-
-  handleClick() {
-    this.setState(
-      {
-        buttonCounter: this.state.buttonCounter + 1
-      }
-    );
+function ColorCounter({ counter, setCount }) {
+  if (counter >= 18) {
+    return <button className="eighteen" onClick={setCount}>Click Me</button>;
+  } else if (counter >= 15) {
+    return <button className="fifteen" onClick={setCount}>Click Me</button>;
+  } else if (counter >= 12) {
+    return <button className="twelve" onClick={setCount}>Click Me</button>;
+  } else if (counter >= 9) {
+    return <button className="nine" onClick={setCount}>Click Me</button>;
+  } else if (counter >= 6) {
+    return <button className="six" onClick={setCount}>Click Me</button>;
+  } else if (counter >= 3) {
+    return <button className="three" onClick={setCount}>Click Me</button>;
+  } else {
+    return <button onClick={setCount}>Click Me</button>;
   }
 }
