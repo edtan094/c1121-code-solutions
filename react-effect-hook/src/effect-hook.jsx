38ccf3accc6af16700c73323f79341cd4dc95924
edtan094 <etan094@gmail.com>
2022-03-24 16:47:59 -0700

    Practiced using effect hooks.

---
+++ react-effect-hook/src/effect-hook.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
+export default function WindowWidth(props) {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  });
+
+  return (
+    <div>{windowWidth}</div>
+  );
+}
---
+++ react-effect-hook/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import WindowWidth from './effect-hook';
+
+ReactDOM.render(
+  <WindowWidth />,
+  document.querySelector('#root')
+);
